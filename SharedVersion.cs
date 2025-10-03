7aebda37f48fec7a573f9b92a3c0c667234f5317
Joe Holt <joe07734@gmail.com>
2025-10-03 12:44:57 +0000

    version

--- SharedVersion.cs
+++ SharedVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("10.11.0a")]
-[assembly: AssemblyFileVersion("10.11.0a")]
+[assembly: AssemblyVersion("10.11.1")]
+[assembly: AssemblyFileVersion("10.11.1")]
